e5daa89ccf519dabca156e374c01f483720dbf0c
gon <valerianaman@gmail.com>
2023-11-03 07:22:03 +0100

    Custom cell type

---
+++ Homepwner/ItemCell.swift
@@ -0,0 +1,17 @@
+//
+//  ItemCell.swift
+//  Homepwner
+//
+//  Created by Gonzalo Benítez Bueno on 2/11/23.
+//
+
+import UIKit
+
+class ItemCell: UITableViewCell{
+    
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var serialNumberLabel: UILabel!
+    @IBOutlet var valueLabel: UILabel!
+    
+    
+}
--- Homepwner/ItemsViewController.swift
+++ Homepwner/ItemsViewController.swift
@@ -53,14 +53,17 @@ class ItemsViewController: UITableViewController{
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
-//        let cell = UITableViewCell(style: .value1, reuseIdentifier: "UITableviewCell")
-        let cell = tableView.dequeueReusableCell(withIdentifier: "UITableViewCell", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ItemCell", for: indexPath) as! ItemCell
         
         
         let item = itemStore.allItems[indexPath.row]
         
-        cell.textLabel?.text = item.name
-        cell.detailTextLabel?.text = "$\(item.valueInDollars)"
+        //cell.textLabel?.text = item.name
+        //cell.detailTextLabel?.text = "$\(item.valueInDollars)"
+        
+        cell.nameLabel.text = item.name
+        cell.serialNumberLabel.text = item.serialNumber
+        cell.valueLabel.text = "$\(item.valueInDollars)"
         
         if item == itemStore.allItems.last{
             cell.isUserInteractionEnabled = false
@@ -110,6 +113,19 @@ class ItemsViewController: UITableViewController{
         super.viewDidLoad()
         
         lastItem = itemStore.allItems.last
+        
+        let statusBarHeight = view.window?.windowScene?.statusBarManager?.statusBarFrame.height ?? 0
+        
+        
+        let insets = UIEdgeInsets(top: statusBarHeight, left: 0, bottom: 0, right: 0)
+        tableView.contentInset = insets
+        tableView.scrollIndicatorInsets = insets
+        
+        tableView.rowHeight = UITableView.automaticDimension
+        tableView.estimatedRowHeight = 65
+        
+        
+        
     }
     
     
