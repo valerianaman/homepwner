9780e4198b887e7179157d32573e8d14c1721ce0
gon <valerianaman@gmail.com>
2023-11-13 16:16:42 +0100

    view in landscape of Details screen

--- Homepwner/SceneDelegate.swift
+++ Homepwner/SceneDelegate.swift
@@ -34,21 +34,27 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // This occurs shortly after the scene enters the background, or when its session is discarded.
         // Release any resources associated with this scene that can be re-created the next time the scene connects.
         // The scene may re-connect later, as its session was not necessarily discarded (see `application:didDiscardSceneSessions` instead).
+        print(#function)
     }
 
     func sceneDidBecomeActive(_ scene: UIScene) {
         // Called when the scene has moved from an inactive state to an active state.
         // Use this method to restart any tasks that were paused (or not yet started) when the scene was inactive.
+        print(#function)
+
     }
 
     func sceneWillResignActive(_ scene: UIScene) {
         // Called when the scene will move from an active state to an inactive state.
         // This may occur due to temporary interruptions (ex. an incoming phone call).
+        print(#function)
     }
 
     func sceneWillEnterForeground(_ scene: UIScene) {
         // Called as the scene transitions from the background to the foreground.
         // Use this method to undo the changes made on entering the background.
+        print(#function)
+
     }
 
     func sceneDidEnterBackground(_ scene: UIScene) {
